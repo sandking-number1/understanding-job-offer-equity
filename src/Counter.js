e840d689eccc0b6b1f3733b09a9424f46492cd18
sandking-number1 <Alfred.J9412@gmail.com>
2017-03-07 18:19:01 -0800

    Displaying percent owned after 1 year and after full vest

--- src/Counter.js
+++ src/Counter.js
@@ -7,14 +7,15 @@ class Counter extends React.Component {
       count: 0,
       initialEmployeeShares: 0,
       initialCompanyShares: 0,
+      vestingPeriod: 0,
       cliffPeriod: 0
     };
 
     this.allMessages = [
       'How many shares have you been issued?',
       'Great. How many total outstanding shares are there in the company?',
-      'At how many months is your cliff? (Enter 12 for 1 year)'
-
+      'Over how many years will your stocks vest?',
+      'Finally, at how many months is your cliff? (Enter 12 for 1 year)'
     ];
     this.handleClick = this.handleClick.bind(this);
   }
@@ -34,24 +35,39 @@ class Counter extends React.Component {
         })
       } else if (this.state.count === 2) {
         this.setState({
+          vestingPeriod: e.target.value,
+          count: this.state.count+1
+        })
+      } else if (this.state.count === 3) {
+        this.setState({
           cliffPeriod: e.target.value,
           count: this.state.count+1
         })
       }
       e.target.value = '';
     }
-    console.log('ok so here is state: ', this.state)
   }
 
+
   render() {
-    return (
-      <div>
-        <h1 id='title'>Understand your equity as a new employee</h1>
-        <input onKeyUp={this.handleClick}></input>
-        <button></button>
-        <div>{this.allMessages[this.state.count]}</div>
-      </div>
-    );
+    if (this.state.count < 4) {
+      return (
+        <div>
+          <h1 id='title'>Understand your equity as a new employee</h1>
+          <input onKeyUp={this.handleClick}></input>
+          <button></button>
+          <div>{this.allMessages[this.state.count]}</div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div>You will own {this.state.initialEmployeeShares/this.state.initialCompanyShares/this.state.vestingPeriod*100}% of the company after one year</div>
+          <div>And it will take you the full vesting schedule of {this.state.vestingPeriod} years to own {this.state.initialEmployeeShares/this.state.initialCompanyShares*100}% of the company</div>
+        </div>
+      );
+    }
+
   }
 }
 export default Counter;
