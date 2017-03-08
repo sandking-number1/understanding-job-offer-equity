5be9dcc2518da5ef6f97282ffa26c9437d8be1b9
sandking-number1 <Alfred.J9412@gmail.com>
2017-03-07 17:46:26 -0800

    Initial form working, and updating state

--- src/Counter.js
+++ src/Counter.js
@@ -4,11 +4,16 @@ class Counter extends React.Component {
   constructor() {
     super();
     this.state = {
-      initialShares: 0,
-      currentInstruction: 'How many shares have you been issued?'
+      count: 0,
+      initialEmployeeShares: 0,
+      initialCompanyShares: 0,
+      cliffPeriod: 0
     };
 
     this.allMessages = [
+      'How many shares have you been issued?',
+      'Great. How many total outstanding shares are there in the company?',
+      'At how many months is your cliff? (Enter 12 for 1 year)'
 
     ];
     this.handleClick = this.handleClick.bind(this);
@@ -17,13 +22,25 @@ class Counter extends React.Component {
   handleClick(e) {
     e.preventDefault();
     if (e.key === 'Enter') {
-      this.setState({
-        initialShares: e.target.value,
-        currentInstruction: 'Great. Lets get money'
-      });
+      if (this.state.count === 0) {
+        this.setState({
+          initialEmployeeShares: e.target.value,
+          count: this.state.count+1
+        });
+      } else if (this.state.count === 1) {
+        this.setState({
+          initialCompanyShares: e.target.value,
+          count: this.state.count+1
+        })
+      } else if (this.state.count === 2) {
+        this.setState({
+          cliffPeriod: e.target.value,
+          count: this.state.count+1
+        })
+      }
       e.target.value = '';
     }
-
+    console.log('ok so here is state: ', this.state)
   }
 
   render() {
@@ -32,7 +49,7 @@ class Counter extends React.Component {
         <h1 id='title'>Understand your equity as a new employee</h1>
         <input onKeyUp={this.handleClick}></input>
         <button></button>
-        <div>{this.state.currentInstruction}</div>
+        <div>{this.allMessages[this.state.count]}</div>
       </div>
     );
   }
